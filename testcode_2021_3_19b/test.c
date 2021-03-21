df5eb066e17ff0063361db2c42d1169c1f872e42
fuwafuwa <ytx070109@gmail.com>
2021-03-21 08:41:31 +0800

    19号的测试代码补发

---
+++ testcode_2021_3_19b/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	//计算两个数的和
+	//变量的使用
+	int num1 = 0;//像内存申请4个字节的空间 并且把这个内存空间挂一个门牌号叫num1
+	int num2 = 0;
+	int sum = 0;
+	//输入数据-使用输入函数scanf
+	scanf("%d%d", &num1, &num2);//&是取地址符号 意思是把一个数据存储到门牌号叫num1的内存空间里
+	//int sum = 0; 如果在此处定义则会报错 因为c99语法标准规定变量定义应该在开头 
+	sum = num1 + num2;
+	printf("sum=%d\n", sum);
+	return 0;
+}
