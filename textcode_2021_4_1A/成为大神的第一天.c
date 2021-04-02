df9211450bdea6442ad0b509567539e8fdea1314
fuwafuwa <ytx070109@gmail.com>
2021-04-02 20:01:48 +0800

    一次课

---
+++ textcode_2021_4_1A/成为大神的第一天.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int ads = 1;
+int main()
+{
+	printf("我要成为大神\n");
+	printf("%d\n", sizeof(long double));
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("a+b=%d\n", sum);
+	int ads = 100;
+	printf("%d\n", ads);
+	return 0;
+}
