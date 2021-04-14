ba516f7c32bfec5bca8b3a4d71dad93567b09c7f
fuwafuwa <ytx070109@gmail.com>
2021-04-14 12:44:49 +0800

    goto语句使用案例

---
+++ testcode_2021_4_14a/testcode_2021_4_14a.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+    int x, y, z;
+    for (x = 0; x <= 100 / 5; x++)
+        for (y = 0; y <= 100 / 3; y++)
+            for (z = 0; z <= 100 * 3; z++)
+            {
+                if ((x * 5 + y * 3 + z / 3 == 100) && (z % 3 == 0) && (x + y + z == 100))
+                {
+                    printf("x=%d y=%d z=%d\n", x, y, z);
+                }
+                else
+                    goto error;
+            }
+error:
+    return 0;
+}
