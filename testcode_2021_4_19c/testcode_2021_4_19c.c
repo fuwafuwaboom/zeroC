8e0799de95d6de2ed1b3f02b3c2064325058c21c
fuwafuwa <ytx070109@gmail.com>
2021-04-19 17:14:00 +0800

    函数的嵌套调用和链式访问

---
+++ testcode_2021_4_19c/testcode_2021_4_19c.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//void laugh1()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 3; i++)
+//		printf("¹þ¹þ ");
+//}
+//void laugh2()
+//{
+//	int j = 0;
+//	for (j = 1; j <= 3; j++)
+//	{
+//		laugh1();
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	laugh2();
+//	return 0;
+//}
+int main()
+{
+	printf("%d", printf("%d", printf("%d", 43)));
+	return 0;
+}
