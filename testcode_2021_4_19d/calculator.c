c44a94fdadee167e0ed8313acec08efa8ce8b8f0
fuwafuwa <ytx070109@gmail.com>
2021-04-19 18:05:02 +0800

    静态库封装函数

---
+++ testcode_2021_4_19d/calculator.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include"add.h"
+int main()
+{
+	int a = 20;
+	int b = 10;
+	printf("%d\n",add(a,b));
+	return 0;
+}
---
+++ testcode_2021_4_19d/div.c
@@ -0,0 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+int div(int x, int y)
+{
+	return x / y;
+}
