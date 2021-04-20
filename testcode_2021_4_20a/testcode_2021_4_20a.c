aff02a165ddd8cf57255a5269b94265121f715b2
fuwafuwa <ytx070109@gmail.com>
2021-04-20 09:46:26 +0800

    递归函数————从小到大输出数

---
+++ testcode_2021_4_20a/testcode_2021_4_20a.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void print(unsigned int n)
+{
+	printf("%d", n % 10);
+	if (n > 9)
+	{
+		print(n / 10);
+	}
+}
+int main()
+{
+	unsigned int num = 0;
+	scanf("%u", &num);
+	print(num);
+	return 0;
+}
