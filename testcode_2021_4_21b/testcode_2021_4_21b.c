0dba46539f312bfbb33b085157c2029864ac94c2
fuwafuwa <ytx070109@gmail.com>
2021-04-21 15:41:42 +0800

    迭代实现逆序排列

---
+++ testcode_2021_4_21b/testcode_2021_4_21b.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int my_strlen(char* str)
+{
+	int count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+void reverse_string(char* str)
+{
+	int left = 0;
+	int right = my_strlen(str) - 1;
+	while (left < right)
+	{
+		char tmp = *(str + left);
+		*(str + left) = *(str + right);
+		*(str + right) = tmp;
+		left++;
+		right--;
+	}
+}
+int main()
+{
+	char arr[100];
+	scanf("%s", arr);
+	reverse_string(arr);
+	printf("%s", arr);
+}
