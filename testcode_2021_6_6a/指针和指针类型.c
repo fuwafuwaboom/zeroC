7405b4e2b82ba2a52ccf7be9df987455b8614429
fuwafuwa <ytx070109@gmail.com>
2021-06-06 10:21:18 +0800

    指针 内存调试

---
+++ testcode_2021_6_6a/指针和指针类型.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int a = 10;//a占4个字节
+//	int* pa = &a;//拿到的是a四个字节的首地址
+//	pa用于存放a的地址，类型是int* ,pa称为指针变量/地址变量
+//	
+//
+//}
+int main()
+{
+	int a = 0x12345678;
+	return 0;
+}
+//通过调试窗口的内存监视 可以不断实验得到内存和地址的规律！
