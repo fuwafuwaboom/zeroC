70742a9ef61b420d0a10087a3ce338175781645a
fuwafuwa <ytx070109@gmail.com>
2021-06-12 13:55:34 +0800

    指针数组

---
+++ testcode_2021_6_12c/指针数组.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//指针数组 ——本质上是一个数组
+//先了解概念，识别即可，应用后面学
+int main()
+{
+	int arr[10];//整型数组 - 存放整型的数组
+	char ch[5];//字符数组 - 存放字符的数组
+	int* parr[10];//指针数组 - 存放整型指针的数组
+	char* pch[5];//字符指针数组
+	return 0;
+}
