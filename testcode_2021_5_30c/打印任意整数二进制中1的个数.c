7cdee65116479bf945172f14d1cdbf9a0438f800
fuwafuwa <ytx070109@gmail.com>
2021-06-14 20:11:44 +0800

    求任意整数二进制序列中1的个数 初级版+改进版

--- testcode_2021_5_30c/打印任意整数二进制中1的个数.c
+++ testcode_2021_5_30c/打印任意整数二进制中1的个数.c
@@ -1,23 +1,38 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-void method1()
+void method1(int a)
+{
+	int i = 0;
+	int sum = 0;
+	while (i < 32)//不可以写成 a != 0 ,因为这个条件不适用于负数！
+	{
+		int c = a & 1;
+		sum += c;
+		a = a >> 1;
+		i++;
+	}
+	printf("%d\n", sum);
+	return;
+}
+
+void method2(int n)
+{
+	int count = 0;
+	int i = 0;
+	while (n)
+	{
+		n &= (n - 1);//n&(n-1)就可以自动实现每次消去右边的一个1
+		count++;
+	}
+	printf("%d\n", count);
+	return;
+	//同理，count == 1 可以判断n是否是2的倍数
+}
+int main()
 {
 	int a = 0;
 	scanf("%d", &a);
-	int b = 1;
-	int d = 0;
-	while (0 != a)
-	{
-		int c = a & b;
-		d = d + c;
-		a = a >> 1;
-	}
-	printf("%d", d);
-	return 0;
-}
-
-int main()
-{
-	method1();
+	method1(a);
+	method2(a);
 	return 0;
 }
