8199ab2da7b93b43610888a5f0fcaa5b1c14ce5c
fuwafuwaboom <ytx070109@gmail.com>
2021-10-02 00:23:28 +0800

    数据的存储blog

---
+++ testcode_2021_9_30/数据在内存中的存储.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	float a = 5.5;
+	return 0;
+}
+
+
+
+
+
+/*int n = 9;
+	float* pFloat = (float*)&n;
+	printf("n的值为：%d\n", n);
+	printf("pFloat的值为：%f\n", *pFloat);
+
+	*pFloat = 9.0;
+	printf("n的值为：%d\n", n);
+	printf("pFloat的值为：%f\n", *pFloat);*/
